e6ba712a2614a61026a1b1b33d786b9a8c6ed17b
Oscar Vallejo <oscar.vallejo@outlook.com>
2024-11-25 10:26:36 +0100

    Exercise 22 solved

--- src/main/java/com/campusdual/classroom/Exercise22.java
+++ src/main/java/com/campusdual/classroom/Exercise22.java
@@ -6,21 +6,34 @@ import java.util.Set;
 
 public class Exercise22 {
 
-    public static List<String> createArrayList() {
+    private static final String[] ELEMENTS = {"GKFFD", "TNANA", "MPMSL", "PSWME", "LZMLF", "JYEBV", "YELNT", "JSNKR", "JFESF", "TMJLL"};
 
+    public static List<String> createArrayList() {
+      List<String> arrayReturned = new ArrayList<>();
+
+       for(String element : ELEMENTS){
+           arrayReturned.add(element);
+       }
+        return arrayReturned;
     }
 
     public static void printElementsAndIndex(List<String> customList) {
 
+        for(int i = 0; i<customList.size(); i++){
+                System.out.println("Índice" + i + "Valor:" + customList.get(i));
+
+            }
     }
 
     public static boolean addElementToList(List<String> customList, String element) {
-
+        return customList.add(element);
     }
 
     public static void main(String[] args) {
+        List<String> arrayStringType = createArrayList();
+        printElementsAndIndex(arrayStringType);
+        addElementToList(arrayStringType, "AAAAA");
+        printElementsAndIndex(arrayStringType);
 
     }
-
-
 }
